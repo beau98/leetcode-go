d06b01fe3c74d47cf625d22a98ec170fcf35c6bf
beau <codemeifan@gmail.com>
2020-10-25 22:23:29 +0800

    top150-001 两数和

---
+++ top150/leetcode-002.go
@@ -0,0 +1,18 @@
+package top150
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+
+}
