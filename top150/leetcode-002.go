081342237d0374b323dd25a3881fd094ab2e5bf8
fan_mei <fan_mei2@sui.com>
2020-10-26 14:55:37 +0800

    add Two Numbers

--- top150/leetcode-002.go
+++ top150/leetcode-002.go
@@ -1,19 +1,52 @@
 package top150
 
 /**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order,
+and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example 1:
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+
+Example 2:
+Input: l1 = [0], l2 = [0]
+Output: [0]
+
+Example 3:
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+
+big int add
+*/
 
 type ListNode struct {
-	val  int
+	Val  int
 	Next *ListNode
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	//
-	return &ListNode{0, nil}
+	temp := 0
+	head := &ListNode{0, nil}
+	nodeCur := head
+	for l1 != nil || l2 != nil {
+		sum := temp
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+		temp = sum / 10
+		nodeCur.Next = &ListNode{sum % 10, nil}
+		nodeCur = nodeCur.Next
+	}
+	if temp != 0 {
+		nodeCur.Next = &ListNode{temp, nil}
+	}
+	return head.Next
 }
